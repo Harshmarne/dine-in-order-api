8799c844a226544f7f2b278a7e2af86c9712746b
Harshmarne <harshmarne85@gmail.com>
2025-02-28 01:10:14 +0530

    Add @Repositry anotation in repo file & userservice inside writen userrole logic and controler responsestructure return in one line

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -2,14 +2,14 @@ package com.example.dio.controller;
 
 import com.example.dio.model.User;
 import com.example.dio.service.UserService;
+import com.example.dio.service.impl.UserServiceImpl;
+import com.example.dio.utility.ResponseBuilder;
 import com.example.dio.utility.ResponseStructure;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import lombok.*;
 
 @RestController
 @AllArgsConstructor
@@ -17,15 +17,10 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/registar")
-    public ResponseEntity<ResponseStructure<User>> registarUser(@RequestBody User user){
+    @PostMapping("/register")
+    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user) {
         user = userService.registar(user);
-
-        ResponseStructure<User> structure = new ResponseStructure<>();
-        structure.setData(user);
-        structure.setStatus(HttpStatus.CREATED.value());
-        structure.setMessage("User Object Created Successfully!!");
-
-        return new ResponseEntity<ResponseStructure<User>>(structure , HttpStatus.CREATED);
+        return ResponseBuilder.success(HttpStatus.CREATED, "User Created", user);
     }
+
 }
