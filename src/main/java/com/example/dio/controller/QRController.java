5bc10c3441a86567dc4057d2a49f7a0f0df91abc
Harshmarne <harshmarne85@gmail.com>
2025-03-13 11:51:46 +0530

    Genrated a QR By Url

---
+++ src/main/java/com/example/dio/controller/QRController.java
@@ -0,0 +1,35 @@
+package com.example.dio.controller;
+
+
+import com.example.dio.service.QRCodeGeneratorService;
+import com.google.zxing.WriterException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("${app.base-url}")
+@AllArgsConstructor
+public class QRController {
+
+    private final QRCodeGeneratorService qrCodeGeneratorService;
+
+    @GetMapping(value = "/qr", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateQRCode(@RequestParam("url") String url) {
+        try {
+            // Generate a 250x250 QR code image for the provided URL
+            byte[] qrImage = qrCodeGeneratorService.generateQRCodeImage(url);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(qrImage);
+        } catch (WriterException | IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
