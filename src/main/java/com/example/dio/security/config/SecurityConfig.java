2d61d73cdefbe02bac141fc45579c01ee4f8e837
Harshmarne <harshmarne85@gmail.com>
2025-03-19 13:35:32 +0530

    Have To add Spring Security in our Application add Dependancy as well as Configer the DaoAuthentication provider and password encoder

---
+++ src/main/java/com/example/dio/security/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.dio.security.config;
+
+import com.example.dio.config.AppEnv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private AppEnv appEnv;
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        String baseUrl = appEnv.getBaseUrl();
+        return security.csrf(csrf -> csrf.disable())
+                .securityMatchers(match -> match.requestMatchers(baseUrl + "/**")) //Basically used to configure filter chain to accept request
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers(
+                        baseUrl+"/register",
+                        baseUrl+"/restaurant/{restaurantId}/food-items"
+                ).permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
